c1999ebf5825cdb287ac9726c9c555226184d854
Denilson Sá <denilsonsa@gmail.com>
2010-01-15 04:27:23 -0200

    Added lots of comments...

--- recursive_division.py
+++ recursive_division.py
@@ -21,7 +21,16 @@ from matplotlib import cm  # colormaps
 
 
 def f1(img, randfunc=random.random):
-    '''First try... This one does not quite work, but it is still pretty anyway. :)'''
+    '''First try. This one does not quite work, but it is still pretty anyway.
+    :)
+
+    Since this was my first try, I got some problems at the recursion. Thus,
+    this code ends up stopping the recursion a bit earlier than should, which
+    explains why many pixels are not touched.
+
+    On the other hand, somehow the resulting image looks like Ordered Dithering
+    http://en.wikipedia.org/wiki/Ordered_dithering
+    '''
     if max(img.shape)<=2:
         return
     mx = img.shape[0]/2
@@ -35,7 +44,21 @@ def f1(img, randfunc=random.random):
 
 
 def f2(img, randfunc=random.random):
-    '''Second try...'''
+    '''Second try. This implements exactly what was written at IMSMap
+    description (except that the corner pixels were not randomized).
+
+    Here, I added some logic to prevent recursive calls with the exact same
+    sub-matrix. This way, the recursive calls go all way down, and all pixels
+    are calculated (except, maybe?, the corner pixels).
+
+    If the random function just adds a value between 0.0 and 1.0, then we get
+    a weird (but pretty interesting) pattern where the top and left edges are
+    quite low, and everything else is quite high. Some artifacts are also very
+    visible, if the original image dimensions are not powers of two.
+
+    On the other hand, if the random function adds a value between -0.5 to 0.5,
+    then we get a cloud noise very similar to the IMSMap xscreensaver.
+    '''
     if max(img.shape)<=1:
         return
     tx = img.shape[0]  # total x
@@ -53,14 +76,28 @@ def f2(img, randfunc=random.random):
     ]
     for (ax,bx),(ay,by) in cuts:
         #print (ax,bx),(ay,by)
-        if (ax < bx or ay < by) and not (
-            ax == 0 and bx == tx and ay == 0 and by == ty
+        if ( # size must be greater than 0
+            (ax < bx and ay < by)
+            and not  # this cut must not be equal to the current full img
+            (ax == 0 and bx == tx and ay == 0 and by == ty)
         ):
             f2(img[ax:bx, ay:by], randfunc)
 
 
 def f3(img, randfunc=random.random):
-    '''Third try...'''
+    '''Third try. This time I noticed that f2() calculates the mean of the four
+    corners, but does not calculate the mean at the edges. Thus, each recursive
+    call has two corners with calculated values, and two others that are
+    zero.
+
+    This f3() implementation tries to address this issue, by calculating the
+    five points: four median points (one on each edge), and the median point of
+    the rectangle.
+
+    Unfortunately, the resulting image is not a very smooth noise. We can
+    clearly notice high values concentrating at some edges (depending on the
+    image dimensions), as well as orthogonal or diagonal artifacts.
+    '''
     if max(img.shape)<=2:
         return
     tx = img.shape[0]  # total x
@@ -88,10 +125,19 @@ def f3(img, randfunc=random.random):
     ]
     for (ax,bx),(ay,by) in cuts:
         #print (ax,bx),(ay,by)
-        if (ax < bx or ay < by) and not (
-            ax == 0 and bx == tx and ay == 0 and by == ty
+        if ( # size must be greater than 0
+            (ax < bx and ay < by)
+            and not  # this cut must not be equal to the current full img
+            (ax == 0 and bx == tx and ay == 0 and by == ty)
         ):
             f3(img[ax:bx, ay:by], randfunc)
+
+
+# Possible improvements:
+#   I've measured that both f2() and f3() recalculate the same pixel more than
+#   once. This probably is the reason for the unexpected patterns and
+#   artifacts. Another implementation that tries to solve this issue would be a
+#   good next stop for this little experimental project.
 
 
 
@@ -120,6 +166,7 @@ def main():
         print funcname
         img = zeros(size)
         func(img, randfunc)
+        print "min=%f, max=%f" % (img.min(), img.max())
         #pyplot.imshow(img, cmap=colormap)
         #pyplot.show()
         pyplot.imsave(fname=funcname+".png", arr=img, cmap=colormap)
